e0d7b4f6889cb39356e9c5b91d90102d19248cac
snehghose <snhghose1313@gmail.com>
2020-11-13 17:23:59 +0530

    angular

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/movie-booking-app'));
+app.use(express.static(__dirname + '/dist/movie-booking-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/movie-booking-app/index.html'));
+res.sendFile(path.join(__dirname + '/dist/movie-booking-app/index.html'));
 });
 app.listen(process.env.PORT || 8085);
